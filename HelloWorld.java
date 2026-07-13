0838ac0f4c4ca6265a8a9500970e73b7feb6759a
Mesanda Sethumika <sethumikamesanda@gmail.com>
2026-07-14 00:13:15 +0530

    Create HelloWorld.java
    Co-Authored-By: sethumikahettiarachi-jpg <304564221+sethumikahettiarachi-jpg@users.noreply.github.com>

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name = "Mesanda";
+        System.out.println("Hello, " + name + "!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Count: " + i);
+        }
+    }
+}//// test uhuuggug
